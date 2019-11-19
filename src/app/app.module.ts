e136754d0a3938fe330c50ea7e6d42e38685ebb8
lcarlso2 <lcarlso2@my.westga.edu>
2019-11-19 17:24:02 -0500

    Added my three basic components. Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu/menu.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { PcBuildComponent } from './pcs/build/pc-build.component';
+import { CreatePcPartComponent } from './pcs/part/create-pc-part.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    PcBuildComponent,
+    CreatePcPartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'builds', component: PcBuildComponent},
+      {path: 'create', component: CreatePcPartComponent},
+      {path : '', redirectTo:'home', pathMatch:'full'},
+      {path : '**', redirectTo:'home', pathMatch:'full'}
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pcs/part/create-pc-part.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-pc-part',
+  templateUrl: './create-pc-part.component.html',
+  styleUrls: ['./create-pc-part.component.css']
+})
+export class CreatePcPartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
